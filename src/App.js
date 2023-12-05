318d108858e4c8a66d2b3ccf90954c566f98e350
Tae Park <tae.park@evernorth.com>
2023-12-05 10:54:48 -0500

    3rd commit 6.6 final

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
+import Home from './Home';
+//import CurrencyConverter from './CurrencyConverter';
+
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/exchange-rate-site">
       <Layout>
         <Switch>
-           <Route path="/" exact render={() => <h1>Hello world</h1>} />
-           <Route render={() => <h1>404 Not found</h1>} />
+          <Route path="/" exact component={Home} />
+          <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
     </Router>
@@ -18,5 +21,3 @@ const App = () => {
 }
 
 export default App;
-
-
