85fba7c3944edd34cf62285009182dc31ec9128d
Tae Park <tae.park@evernorth.com>
2023-12-07 13:15:40 -0500

    5th commit 6.8 final

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
 import Home from './Home';
-//import CurrencyConverter from './CurrencyConverter';
+import CurrencyConverter from './CurrencyConverter';
 
 import './App.css';
 
@@ -13,6 +13,7 @@ const App = () => {
       <Layout>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/currencyconverter" component={CurrencyConverter} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
